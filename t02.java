d08c69cada9005217c58dc7ca9d8eb7f902383e8
spookyjjj <104603810+spookyjjj@users.noreply.github.com>
2022-05-18 17:25:56 +0900

    Delete t02.java

--- t02.java
+++
@@ -1,9 +0,0 @@
-// 두 수 44와 72의 + - * 각각의 연산결과를 출력해보세요.
-
-public class t02 {
-    public static void main(String args[]){
-        System.out.println(44 + 72);
-        System.out.println(44 - 72);
-        System.out.println(44 * 72);
-    }
-}

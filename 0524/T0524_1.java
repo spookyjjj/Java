6fdcdca543da24a07f44ae9cc59ce6fb91fe1fe5
spookyjjj <104603810+spookyjjj@users.noreply.github.com>
2022-05-25 17:10:57 +0900

    문제풀이

--- 0524/T0524_1.java
+++ 0524/T0524_1.java
@@ -11,50 +11,80 @@ public class T0524_1 {
 		
 		//1. 369게임 : 1 2 Fizz 4 Buzz Fizz 7 8 Buzz ..FizzBuzz(15).. ~35까지
 		//1에서 35까지 반복 -> 3의배수이면서 5의배수이면 FizzBuzz -> 3의배수이면 Fizz -> 5의배수이면 Buzz -> 암것도 아니면 걍 출력
-		// for (int i = 1; i <= 35; i++) {
-			// if(i % 3 == 0 && i % 5 == 0){
-				// System.out.println("FizzBuzz");
-			// } else if (i % 3 == 0) {
-				// System.out.println("Fizz");
-			// } else if (i % 5 == 0) {
-				// System.out.println("Buzz");
-			// } else {
-				// System.out.println(i);
-			// }
-		// }
-		// System.out.println("369종료");
+		for (int i = 1; i <= 35; i++) {
+			if(i % 3 == 0 && i % 5 == 0){
+				System.out.println("FizzBuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
+		System.out.println("369종료");
 		
 		//2. Fibonacci 수열 : 1 1 2 3 5 8 13 21 ... ~100이하까지
-		// int a = 1;
-		// int b = 1;
-		// int c;
-		// System.out.println(a);
-		// System.out.println(b);
-		// while (b <= 100) {
-			// c = a; //a값 변하기 전에 저장
-			// a = b;
-			// b = c + b;
-			// if (b > 100) {
-				// continue;
-			// }
-			// System.out.println(b);
-		// }
-		// System.out.println("수열종료");
+		int a = 1;
+		int b = 1;
+		int c;
+		System.out.println(a);
+		System.out.println(b);
+		while (b <= 100) {
+			c = a; //a값 변하기 전에 저장
+			a = b;
+			b = c + b;
+			if (b > 100) {
+				continue;
+			}
+			System.out.println(b);
+		}
+		System.out.println("수열종료");
+		/* ★학우답안★
+		int a = 1;
+		int b = 1;
+		int c
+		System.out.println(b);
+		while (b <= 100) {
+			System.out.println(b);
+			c = a + b; //c는 a+b만 넣어 놓는 용도
+			a = b;
+			b = c;
+		}
+		*/
 
 		//3. 사용자가 입력한 정수가 소수임을 확인하여 출력하는 프로그램
-		// System.out.print("소수임을 확인 하고 싶은 정수를 입력하세요: ");
-		// int input = scan.nextInt();
+		System.out.print("소수임을 확인 하고 싶은 정수를 입력하세요: ");
+		int input = scan.nextInt();
 		
-		// int count = 0;
-		// for (int i = 1; i <= input; i++) {
-			// if (input % i == 0 ) {
-				// count++;
-			// }
-		// }
-		// System.out.println((count == 2) ? "소수입니다" : "소수가 아닙니다");
+		int count = 0;
+		for (int i = 1; i <= input; i++) {
+			if (input % i == 0 ) {
+				count++;
+			}
+		}
+		System.out.println((count == 2) ? "소수입니다" : "소수가 아닙니다");
 		
 		
 		//4. 사용자에게 비밀번호를 물어보고 동일할 경우 통과 최대 3번의 기회를 줌
+		/* ★★★선생님 답안★★★ -> do while을 쓴것, while조건안에 || && 쓴것, 탈출 후에 if else로 분류한것
+		String pw = "1q2w3e";
+		int pwcount = 3;
+		String pwinput;
+		do {
+			System.out.println("비밀번호를 입력하세요");
+			pwinput = scan.nextLine();
+			pwcount--;
+			System.out.println("현재 카운트값 확인: " + pwcount);
+		} while (!pwinput.equals(pw) && pwcount > 0);
+		
+		if (pwinput.equals(pw)) {
+			System.out.println("로그인");
+		} else {
+			System.out.println("비밀번호 확인 후 다시 시도해 주세요");
+		} */
+		
+		scan.nextLine(); //위에서 nextInt때문에..
 		String pw = "q1w2e3";
 		int pwcount = 0;
 		while (pwcount < 3) {

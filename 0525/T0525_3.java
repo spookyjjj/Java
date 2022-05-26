5aba176dc372a4135c1382dae4da67de3b857f17
spookyjjj <104603810+spookyjjj@users.noreply.github.com>
2022-05-26 17:08:21 +0900

    별..찍기..

--- 0525/T0525_3.java
+++ 0525/T0525_3.java
@@ -74,7 +74,7 @@ public class T0525_3 {
 				// System.out.print("*");
 			// }
 			// System.out.println("");
-		//->근데 이렇게 하면 역순과 정순을 섞을때 j로 묶기가 애매해진다, 정순은 j가 1 2 3커지고 역순은 j가 3 2 1작아지니깐
+		//->이렇게 하면 역순과 정순을 섞을때 j로 묶기가 애매해진다, 정순은 j가 1 2 3커지고 역순은 j가 3 2 1작아지니깐
 		//-> 1 2 3으로 변수가 커지게 고치면 j로 묶기가 쉬워진다
 		// for (int i = 0; i <= 3-1; i++) {
 				// System.out.print("*");
@@ -120,8 +120,8 @@ public class T0525_3 {
 		// }
 		//둘이 더하기
 		for (int j = 1; j <= line; j++) {
-			for (int i = 0; i <= line-j; i++) {
-				System.out.print("  ");
+			for (int i = 1; i <= line-j; i++) {
+				System.out.print(" ");
 			}
 			for (int i = 1; i <= j; i++) {
 				System.out.print("*");
@@ -129,23 +129,23 @@ public class T0525_3 {
 			System.out.println("");
 		}
 		
-		//트리모양은? 공란을 한칸으로 고치기
+		//트리모양은? 별이 홀수개씩 증가
 		for (int j = 1; j <= line; j++) {
-			for (int i = 0; i <= line-j; i++) {
+			for (int i = 1; i <= line-j; i++) {
 				System.out.print(" ");
 			}
-			for (int i = 1; i <= j; i++) {
+			for (int i = 1; i <= (2 * j) - 1; i++) {
 				System.out.print("*");
 			}
 			System.out.println("");
 		}
 		
 		//역순트리는? 공란+별 순서! 공란은 증가, 별은 감소
-		for (int j = 1; j <= line; j++) {
-			for (int i = 1; i <= j; i++) {
+		for (int j = line; j >= 0; j--) {
+			for (int i = 1; i <= line-j; i++) {
 				System.out.print(" ");
 			}		
-			for (int i = 0; i <= line-j; i++) {
+			for (int i = 1; i <= (2 * j) - 1; i++) {
 				System.out.print("*");
 			}
 			System.out.println("");

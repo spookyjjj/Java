ea24b235bd0a34a7d0ee6c3f5ad973b735f13d12
spookyjjj <104603810+spookyjjj@users.noreply.github.com>
2022-06-07 20:44:21 +0900

    문제풀이 추가

--- workspace/220607-02_array/src/Main5.java
+++ workspace/220607-02_array/src/Main5.java
@@ -2,32 +2,6 @@
 //2. 사용자가 0 이하의 수를 입력할 때 까지 반복하여 입력하여. 최근 5개를 역순 출력
 
 import java.util.Scanner;
-class Store {
-	int a;
-	int b;
-	int c;
-	int d;
-	int e;
-
-	public Store() {}
-	
-	public void setA(int a) {
-		this.a = a;
-	}
-	public void setB(int b) {
-		this.b = b;
-	}
-	public void setC(int c) {
-		this.c = c;
-	}
-	public void setD(int d) {
-		this.d = d;
-	}
-	public void setE(int e) {
-		this.e = e;
-	}
-
-}
 
 public class Main5 {
 	public static void main(String[] args) {
@@ -43,7 +17,7 @@ public class Main5 {
 			System.out.println(a[i]);
 		}
 		
-		//2. 사용자가 0 이하의 수를 입력할 때 까지 반복하여 입력하여. 최근 5개를 역순 출력
+		//2-1. 사용자가 0 이하의 수를 입력할 때 까지 반복하여 입력하여. 최근 5개를 역순 출력
 		int[] b = new int[6];
 		while (true) {
 			b[5] = scan.nextInt();
@@ -59,8 +33,18 @@ public class Main5 {
 		for (int i = 4; i >= 0; i--) {
 			System.out.println(b[i]);
 		}
-		//순서가 상관 없다면, int값 5개를 매개변수로 두는 클래스를 하나 만들어서 ...사용자 입력 받을 때 마다 set하구...음수값들어오면 멈추고.. 출력은 void로
-		int x;
-		x = scan.nextInt();
+		//2-2. 순서가 상관 없다면, int값 5개 배열 ..사용자 입력 받을 때 마다 5개 안에서 set..음수값들어오면 멈추고..
+		int[] c = new int[5];
+		Loop : //중첩반복문 전체를 빠져나오기 위해 바깥루프에 이름붙여줌
+		while (true) {
+			int x;
+			for (int i = 0; i < 5; i++) {
+				x = scan.nextInt();
+				if (x <= 0) break Loop; else c[i] = x;
+			}
+		}
+		for (int i = 4; i >= 0; i--) {
+			System.out.println(c[i]);
+		}
 	}
 }

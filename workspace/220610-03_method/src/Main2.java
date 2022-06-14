7b4cc7919e06c78df115ae0f2f1b2af904f87d56
spookyjjj <104603810+spookyjjj@users.noreply.github.com>
2022-06-14 10:00:28 +0900

    스캐너로 입력받은 것 문자열/정수형 구분하기

--- workspace/220610-03_method/src/Main2.java
+++ workspace/220610-03_method/src/Main2.java
@@ -44,12 +44,37 @@ class VendingMachine {
 
 
 public class Main2 {
+	//★문자열을 받아와 숫자면 리턴T, 문자면 리턴F하는 메소드
+	static boolean checkNumber(String str){ 
+		if(str.equals(""))
+		{
+			//문자열이 공백인지 확인
+			return false;
+		}
+		char check;
+		for(int i = 0; i<str.length(); i++){
+			check = str.charAt(i);
+			if( check < 48 || check > 57) //48=0, ... ,57=9 이므로 이 아스키 밖의 문자는 숫자가 아님!
+			{
+				return false;
+			}
+		}		
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		VendingMachine m = new VendingMachine();
 		System.out.println(m.makeCan(1));
 		System.out.println(m.makeCan("cider"));
 		
-		//스캐너로 받아내는 방법?!
+		//★스캐너로 받아내는 방법?! -> boolean checkNumber(String str)라는 메소드 생성해서 써먹으면 됨
+		System.out.print("입력하세요: ");
+		String str = sc.next();
+		if(checkNumber(str)) {
+			System.out.println(m.makeCan(Integer.parseInt(str)));
+		} else {
+			System.out.println(m.makeCan(str));
+		}
 	}
 }
